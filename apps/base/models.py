024b2e7137d3b42174917097ca276d6cb69944e0
Natanael <natanaelsilva.progjs@gmail.com>
2022-09-08 22:45:28 -0300

    feat: BaseMOdel abstract creation

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    status = models.BooleanField("Status", default=True)
+    created_at = models.DateTimeField("Created At", auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField("Updated At", auto_now=True, auto_now_add=False)
+    deleted_date = models.DateTimeField("Deleted", auto_now=True, auto_now_add=False)
+
+    class Meta:
+        abstract = True
+        verbose_name = "BaseModel"
+        verbose_name_plural = "BaseModels"
