436bd5c5a510e7a8e883d4f65369e21c31a388c5
Natanael <natanaelsilva.progjs@gmail.com>
2022-09-09 09:07:15 -0300

    feat: migrations of app base and products

--- apps/products/models.py
+++ apps/products/models.py
@@ -63,3 +63,24 @@ class OfferIndicator(BaseModel):
         return f'Category offer {self.category_product}: {self.discount_value}%'
 
 
+class Product(BaseModel):
+    name = models.CharField("Name of product", max_length=150, blank=False, null=False, unique=True)
+    description = models.CharField("Description", max_length=100, blank=False, null=False)
+    image = models.ImageField("Image of prducts", upload_to="products/", blank=True, null=True)
+
+    historical = HistoricalRecords()
+
+    @property
+    def _history_user(self):
+        return self.changed_by
+
+    @_history_user.setter
+    def _history_user(self, value):
+        self.changed_by = value
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.description
