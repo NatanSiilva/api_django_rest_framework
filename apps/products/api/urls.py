822b402bc5130763886a5bf48c93e91300457340
Natanael <natanaelsilva.progjs@gmail.com>
2022-09-10 15:46:34 -0300

    feat: Implementing viewsets and routers

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
 from apps.products.api.views.general_views import *
-from apps.products.api.views.product_views import *
+from apps.products.api.views.product_viewsets import *
 
 urlpatterns = [
     path("measure_units/", MeasureUnitListAPIView.as_view(), name="measure_units"),
     path("offer_indicators/", OfferIndicatorListAPIView.as_view(), name="offer_indicators"),
     path("category_products/", CategoryProductListAPIView.as_view(), name="category_products"),
-    path("", ProductCreateAPIView.as_view(), name="products_create"),
-    path(
-        "retrieve-update-destroy/<int:pk>/",
-        ProductRetrieveUpdateDestroyAPIView.as_view(),
-        name="products-retrieve-update-destroy",
-    ),
 ]
--- ecomerce_rest/urls.py
+++ ecomerce_rest/urls.py
@@ -5,9 +5,8 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("apps.users.api.urls")),
-    path("api/product/", include("apps.products.api.urls")),
+    path("api/products/", include("apps.products.api.routers")),
 ]
-
 
 urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
